2732272043104bd794f5380c7781dd1044a7ba82
Toomas Romer <toomasr@gmail.com>
2018-04-20 22:14:13 +0300

    Changed layout from two column to minipages

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-# Generate the TeX file
+# Delete old pdflatex files
+rm build/*
 
+# Generate the TeX file
+echo "Outputting file to build/output.tex"
 python src/generate.py
+echo "/Outputting file to build/output.tex"
 
 # Build the PDF
+pdflatex -output-directory=build build/output.tex
 
-pdflatex -output-directory=build build/output.tex 
+# Move result to releases
+cp build/output.pdf releases/
--- src/generate.py
+++ src/generate.py
@@ -8,22 +8,26 @@ templateBlock = "src/templates/default-block.tex"
 templateBlockStr = ""
 
 with open(template, "r") as myfile:
-	templateStr = myfile.read()
+    templateStr = myfile.read()
 
 with open(templateBlock, "r") as myfile:
-	templateBlockStr = myfile.read()
+    templateBlockStr = myfile.read()
 
 images = glob.glob("problems/easy/*.png")
 images.sort()
 
 bodyResult = ""
 counter = 0
-for image in images:
-	bodyResult = bodyResult+templateBlockStr.replace("$PATH", image)+"\n"
-	counter = counter + 1
-	if counter % 6 == 0:
-		counter = 0
-		bodyResult = bodyResult + "\clearpage\n"
+
+for i in range(0, len(images), 2):
+    image1 = images[i]
+    image2 = images[i+1]
+    bodyResult = bodyResult+templateBlockStr.replace("$PATH1",image1).replace("$PATH2", image2)+"\n"
+
+    counter = counter + 1
+    if counter % 3 == 0:
+        counter = 0
+        bodyResult = bodyResult + "\clearpage\n"
 
 completeTexFile = templateStr.replace("$CONTENTS", bodyResult)
 
