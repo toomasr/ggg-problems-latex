98d4835d07e0b68f1ad63602a5fc8a2bc3f2c5fa
Toomas Romer <toomasr@gmail.com>
2018-04-21 08:18:27 +0300

    Also building intermediate and hard problems now

--- build.sh
+++ build.sh
@@ -4,12 +4,16 @@
 rm build/*
 
 # Generate the TeX file
-echo "Outputting file to build/output.tex"
-python src/generate.py
-echo "/Outputting file to build/output.tex"
+python src/generate.py problems/easy/ build/easy.tex
+python src/generate.py problems/intermediate/ build/intermediate.tex
+python src/generate.py problems/hard/ build/hard.tex
 
 # Build the PDF
-pdflatex -output-directory=build build/output.tex
+pdflatex -output-directory=build build/easy.tex
+pdflatex -output-directory=build build/intermediate.tex
+pdflatex -output-directory=build build/hard.tex
 
 # Move result to releases
-cp build/output.pdf releases/
+cp build/easy.pdf releases/easy-problems.pdf
+cp build/intermediate.pdf releases/intermediate-problems.pdf
+cp build/hard.pdf releases/hard-problems.pdf
